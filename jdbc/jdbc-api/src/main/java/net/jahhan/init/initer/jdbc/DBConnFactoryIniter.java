2c325bcb82bd94cccc2906b18ccfb90cd41eed9d
nince <41284688@qq.com>
2018-01-26 17:29:08 +0800

    update

--- jdbc/jdbc-api/src/main/java/net/jahhan/init/initer/jdbc/DBConnFactoryIniter.java
+++ jdbc/jdbc-api/src/main/java/net/jahhan/init/initer/jdbc/DBConnFactoryIniter.java
@@ -2,8 +2,6 @@ package net.jahhan.init.initer.jdbc;
 
 import javax.inject.Inject;
 
-import org.springframework.guice.injector.InjectorHolder;
-
 import net.jahhan.init.BootstrapInit;
 import net.jahhan.init.InitAnnocation;
 import net.jahhan.spi.DataSourcePoolInit;
@@ -17,6 +15,5 @@ public class DBConnFactoryIniter implements BootstrapInit {
 	public void execute() {
 		// 数据库连接池初始化
 		dataSourcePoolInit.init();
-		InjectorHolder.getInstance().getContext().getBeanDefinition("userController");
 	}
 }
